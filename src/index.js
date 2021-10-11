8cbb53e3026882c84bb4bb561d0c26a6babe1507
longlu <longlu@lpcollege.com>
2021-10-11 15:39:42 +0800

    vue-mobile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+ * @Author: 龙露
+ * @Date: 2021-06-01 17:43:04
+ * @LastEditors: 龙露
+ * @LastEditTime: 2021-06-01 17:43:05
+ */
